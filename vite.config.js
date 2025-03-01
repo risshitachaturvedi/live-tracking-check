3730d0fe3d8a2b0ecfd46d995bbb0ba0a10ea94a
risshitachaturvedi <rishitachaturvedi@gmail.com>
2025-03-01 12:03:52 +0530

    deploying to github

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
-// import fs from "fs";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/live-tracking-check",
+  base: "/live-tracking-check/",
   server: {
     host: "0.0.0.0", // Local network access
     port: 5174,
