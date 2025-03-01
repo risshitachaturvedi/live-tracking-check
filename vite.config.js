2b825c99522ea76347856137186c685988350e6c
risshitachaturvedi <rishitachaturvedi@gmail.com>
2025-03-01 11:30:55 +0530

    deploying on github-pages

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/live-tracking-check",
   server: {
-    https: false, //{
-    //   key: fs.readFileSync("C:/Users/PP/Desktop/LiveTracking/key.pem"), // Replace with actual key path
-    //   cert: fs.readFileSync("C:/Users/PP/Desktop/LiveTracking/cert.pem"), // Replace with actual cert path
-    // },
     host: "0.0.0.0", // Local network access
     port: 5174,
   },
